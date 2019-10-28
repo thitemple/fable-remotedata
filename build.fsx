43bb17a56ed00dc759ee403add776a5ae59ad799
Thiago Temple <thiagot@surveymonkey.com>
2019-10-28 11:11:53 -0400

    Updates build script with latest tools

--- build.fsx
+++ build.fsx
@@ -1,9 +1,11 @@
 #r "node_modules/fable-metadata/lib/Fable.Core.dll"
 #load "node_modules/fable-publish-utils/PublishUtils.fs"
+
 open PublishUtils
 
 run "npm test"
+
 match args with
-| IgnoreCase "publish"::_ ->
-    pushNuget "src/Fable.RemoteData.fsproj"
-| _ -> ()
+| IgnoreCase "publish" :: _ -> pushNuget "src/Fable.RemoteData.fsproj"
+
+| _ -> (fun _ -> ())
